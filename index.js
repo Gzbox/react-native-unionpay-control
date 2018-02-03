d68e08ee21887b447377cc3f90e09bb3bc9f9fd5
hanshuai <hanshuai@dongwu-inc.com>
2018-02-03 17:29:28 +0800

    format code

--- index.js
+++ index.js
@@ -2,35 +2,36 @@
 
 import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
 
-const {Unionpay} = NativeModules;
+const { Unionpay } = NativeModules;
 
 let hook = undefined;
 
 const UnionpayEmitter = new NativeEventEmitter(Unionpay)
 const subscription = UnionpayEmitter.addListener('UnionPay_Resp', (resp) => {
-	if(hook != undefined) hook(resp)
-		hook = undefined;
+  if (hook != undefined) hook(resp)
+  hook = undefined;
 })
 
 export function isPaymentAppInstalled() {
-	return new Promise((resolve, reject) => {
-		Unionpay.isPaymentAppInstalled((err, result) => {
-		if (err) reject("unionpay plugin is not installed")
-		else resolve(true)
-		})
-	})
+  return new Promise((resolve, reject) => {
+    Unionpay.isPaymentAppInstalled((err, result) => {
+      if (err) reject("unionpay plugin is not installed")
+      else resolve(true)
+    })
+  })
 }
+
 export function startPay(tn, mode) {
-	return new Promise((resolve, reject) => {
-		Unionpay.startPay(tn, mode, (err, result) => {
-			if (err) reject(err);
-		});
-		hook =  (resp) => {
-			if(resp.code === 'success') {
-				resolve(resp)
-			} else {
-				reject(resp)
-			}
-		};
+  return new Promise((resolve, reject) => {
+    Unionpay.startPay(tn, mode, (err, result) => {
+      if (err) reject(err);
+    });
+    hook = (resp) => {
+      if (resp.code === 'success') {
+        resolve(resp)
+      } else {
+        reject(resp)
+      }
+    };
   });
 }
